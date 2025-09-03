04ade91795e2c095db16fa4f70e4df8bdfbe6166
Fabian Senf <senf@tropos.de>
2025-09-03 22:44:29 +0200

    updates for juwels

--- config/juwels/module_loader.sh
+++ config/juwels/module_loader.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Load necessary modules for Juwels
-module load Stages/2025
-module load GCC OpenMPI
-module load Python
-module load CDO NCO
+module load Stages/2025  GCC/13.3.0  OpenMPI/5.0.5
+module load CDO 
+module load SciPy-Stack netcdf4-python
